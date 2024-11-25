9c868c9b993f0f3435c8d8af5bada5ebdfd07f31
thomasbfairchild <147108362+thomasbfairchild@users.noreply.github.com>
2024-11-25 08:57:08 -0600

    Create number_guesser.py
    Learning Python!

---
+++ number_guesser.py
@@ -0,0 +1,24 @@
+import random
+
+def guess(x):
+    random_number = random.randint(1, x)
+    guess = 0
+    while guess != random_number:
+        guess = int(input(f"Guess a number between 1 and {x}: "))
+        if guess > random_number:
+            print("Too high! Try again! ")
+        elif guess < random_number:
+            print("Too low! Try again! ")
+
+    print(f"Congrats you guess the number {random_number} correctly!")
+
+
+guess(20)
+
+
+
+
+
+
+
+
